fbf762cf86ce68d0ff16dcb2f43e9aa531594502
Nehemaih <113086860+nehemiah213@users.noreply.github.com>
2024-04-02 21:18:46 +0530

    Create viewConsonants.java

---
+++ viewConsonants.java
@@ -0,0 +1,23 @@
+public class VowelConsonantCounter {
+
+    public static void main(String[] args) {
+      String str = "This is a test string";
+      int vowelCount = 0;
+      int consonantCount = 0;
+  
+      str = str.toLowerCase(); // Converting to lower case for beterr understnding of case sensitive 
+  
+      for (char c : str.toCharArray()) {
+        if (c >= 'a' && c <= 'z') {
+          if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            vowelCount++;
+          } else {
+            consonantCount++;
+          }
+        }
+      }
+  
+      System.out.println("The string '" + str + "' has " + vowelCount + " vowels and " + consonantCount + " consonants.");
+    }
+  }
+  
