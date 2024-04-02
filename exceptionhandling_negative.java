959d854f374cee068da881e9a823cc4ae2b52604
Nehemaih <113086860+nehemiah213@users.noreply.github.com>
2024-04-02 21:14:56 +0530

    Create exceptionhandling_negative.java

---
+++ exceptionhandling_negative.java
@@ -0,0 +1,29 @@
+package EXCEPTION_HANDLING_NEGATIVE_NUMBER;
+
+
+public class NumberValidator {
+
+    public static class CustomValidationException extends Exception {
+      public CustomValidationException(String message) {
+        super(message);  // in this code i am taking customvalidecxeption directly in the class by decalring as stactic as it can be acessed by static instaces like objects of the class
+      }
+    }
+  
+    public static void validateNumber(int number) throws CustomValidationException {
+      if (number < 0) {
+        throw new CustomValidationException("Number cannot be negative.");
+      } else {
+        System.out.println("Number is valid.");
+      }
+    }
+  
+    public static void main(String[] args) {
+      int number = -5;
+      try {
+        validateNumber(number);
+      } catch (CustomValidationException e) {
+        System.out.println("Exception caught: " + e.getMessage());
+      }
+    }
+  }
+  
